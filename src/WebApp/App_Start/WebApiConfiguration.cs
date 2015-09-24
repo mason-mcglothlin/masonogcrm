7d9cc29b7eb70329b6ea7dabcedf68cda467279f
Mason McGlothlin <mason.mcglothlin@gmail.com>
2015-09-23 21:51:40 -0500

    Added basic Web Application with Web API for manipulating customers.

---
+++ src/WebApp/App_Start/WebApiConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace MasonOgCRM.WebApp.App_Start
+{
+	public static class WebApiConfiguration
+	{
+		public static void ConfigureWebApi()
+		{
+			RouteTable.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
+}
---
+++ src/WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MasonOgCRM.WebApp.Global" Language="C#" %>
