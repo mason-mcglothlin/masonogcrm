d8e6d9fe66b027a6d18ceedfe5c8c42c3a957f8b
Mason McGlothlin <mason.mcglothlin@gmail.com>
2015-09-23 20:28:10 -0500

    Added documentation for the repository CRUD methods.

--- src/DataAccess.Common/IOgCRMRepository.cs
+++ src/DataAccess.Common/IOgCRMRepository.cs
@@ -8,27 +8,132 @@ using MasonOgCRM.DomainModels;
 
 namespace MasonOgCRM.DataAccess.Common
 {
+	/// <summary>
+	/// Interface that defines the functions an OgCRM repository should implement for CRUD of domain entities.
+	/// </summary>
 	public interface IOgCRMRepository
 	{
+		/// <summary>
+		/// Retrieve a Customer from the repository by Id.
+		/// </summary>
+		/// <param name="id">Id of the customer to retrieve.</param>
+		/// <returns></returns>
 		Task<Customer> FindCustomerByIdAsync(int id);
+
+		/// <summary>
+		/// Retrieve all Customer objects from the repository.
+		/// </summary>
+		/// <returns></returns>
 		Task<List<Customer>> GetAllCustomersAsync();
-		Task<List<Customer>> GetAllCustomers(Expression<Func<bool>> expression);
-		Task DeleteCustomer(int id);
-		Task UpdateCustomer(Customer customer);
-		Task AddCustomer(Customer customer);
 
+		/// <summary>
+		/// Retrieve all Customer objects from the repostory that match the given expression.
+		/// </summary>
+		/// <param name="expression">Expression that returns true for Customer objects to retrieve.</param>
+		/// <returns></returns>
+		Task<List<Customer>> GetAllCustomersAsync(Expression<Func<bool>> expression);
+
+		/// <summary>
+		/// Remove a customer from the repository.
+		/// </summary>
+		/// <param name="id">Id of the customer to remove from the repository.</param>
+		/// <returns></returns>
+		Task DeleteCustomerAsync(int id);
+
+		/// <summary>
+		/// Update a Customer in the repository that has the matching Id with modified property values.
+		/// </summary>
+		/// <param name="customer">Customer object that contains the modified property values to update with.</param>
+		/// <returns></returns>
+		Task UpdateCustomerAsync(Customer customer);
+
+		/// <summary>
+		/// Add a customer to the repository.
+		/// </summary>
+		/// <param name="customer">Customer object to add to the repository.</param>
+		/// <returns></returns>
+		Task AddCustomerAsync(Customer customer);
+
+		/// <summary>
+		/// Retrieve a UserAccount from the repository by Id.
+		/// </summary>
+		/// <param name="id">Id of the UserAccount to retrieve.</param>
+		/// <returns></returns>
 		Task<UserAccount> FindUserAccountByIdAsync(int id);
-		Task<List<UserAccount>> GetAllUserAccountsAsync();
-		Task<List<UserAccount>> GetAllUserAccounts(Expression<Func<bool>> expression);
-		Task DeleteUserAccount(int id);
-		Task UpdateUserAccount(UserAccount userAccount);
-		Task AddUserAccount(UserAccount userAccount);
 
+		/// <summary>
+		/// Retrieve all UserAccount objects from the repository.
+		/// </summary>
+		/// <returns></returns>
+		Task<List<UserAccount>> GetAllUserAccountsAsync();
+
+		/// <summary>
+		/// Retrieve all UserAccount objects from the repository that match the given expression.
+		/// </summary>
+		/// <param name="expression"></param>
+		/// <returns></returns>
+		Task<List<UserAccount>> GetAllUserAccountsAsync(Expression<Func<bool>> expression);
+
+		/// <summary>
+		/// Remove a user account from the repository.
+		/// </summary>
+		/// <param name="id">Id of the user account to remove from the repository.</param>
+		/// <returns></returns>
+		Task DeleteUserAccountAsync(int id);
+
+		/// <summary>
+		/// Update a UserAccount in the repository that has the matching Id with modified property values.
+		/// </summary>
+		/// <param name="userAccount">UserAccoubt object that contains the modified property values to update with.</param>
+		/// <returns></returns>
+		Task UpdateUserAccountAsync(UserAccount userAccount);
+
+		/// <summary>
+		/// Add a user account to the repository.
+		/// </summary>
+		/// <param name="userAccount">UserAccount to add to the repository.</param>
+		/// <returns></returns>
+		Task AddUserAccountAsync(UserAccount userAccount);
+
+		/// <summary>
+		/// Retrieve a Note from the repository by Id.
+		/// </summary>
+		/// <param name="id">Id of the note to retrieve.</param>
+		/// <returns></returns>
 		Task<Note> FindNoteByIdAsync(int id);
+
+		/// <summary>
+		/// Retrieve all Note objects from the repository.
+		/// </summary>
+		/// <returns></returns>
 		Task<List<Note>> GetAllNotesAsync();
-		Task<List<Note>> GetAllNotes(Expression<Func<bool>> expression);
-		Task DeleteNote(int id);
-		Task UpdateNote(Note userAccount);
-		Task AddNote(Note userAccount);
+
+		/// <summary>
+		/// Retrieve all Note objects from the repository that match the given expression.
+		/// </summary>
+		/// <param name="expression">Expression that returns true for Customer objects to retrieve.</param>
+		/// <returns></returns>
+		Task<List<Note>> GetAllNotesAsync(Expression<Func<bool>> expression);
+
+		/// <summary>
+		/// Remove a note from the repository.
+		/// </summary>
+		/// <param name="id">Id of the note to remove from the repository.</param>
+		/// <returns></returns>
+		Task DeleteNoteAsync(int id);
+
+		/// <summary>
+		/// Update a Note in the repository that has the matching Id with modified property values.
+		/// </summary>
+		/// <param name="note">Note object that contains the modified property values to update with.</param>
+		/// <returns></returns>
+		Task UpdateNoteAsync(Note note);
+
+		/// <summary>
+		/// Add a note to the repository.
+		/// </summary>
+		/// <param name="userAccount">Note to add the repository.</param>
+		/// <returns></returns>
+		Task AddNoteAsync(Note note);
 	}
 }
--- src/DataAccess.EFRepository/EntityFrameworkRepository.cs
+++ src/DataAccess.EFRepository/EntityFrameworkRepository.cs
@@ -9,94 +9,184 @@ using MasonOgCRM.DomainModels;
 
 namespace MasonOgCRM.DataAccess.EFRepository
 {
+	/// <summary>
+	/// Repository class for retriving entities using Entity Framework
+	/// </summary>
 	public class EntityFrameworkRepository : IOgCRMRepository
 	{
-		public Task AddCustomer(Customer customer)
+		/// <summary>
+		/// Add a customer to the repository.
+		/// </summary>
+		/// <param name="customer">Customer object to add to the repository.</param>
+		/// <returns></returns>
+		public Task AddCustomerAsync(Customer customer)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task AddNote(Note userAccount)
+		/// <summary>
+		/// Add a note to the repository.
+		/// </summary>
+		/// <param name="userAccount">Note to add the repository.</param>
+		/// <returns></returns>
+		public Task AddNoteAsync(Note note)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task AddUserAccount(UserAccount userAccount)
+		/// <summary>
+		/// Add a user account to the repository.
+		/// </summary>
+		/// <param name="userAccount">UserAccount to add to the repository.</param>
+		/// <returns></returns>
+		public Task AddUserAccountAsync(UserAccount userAccount)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteCustomer(int id)
+		/// <summary>
+		/// Remove a customer from the repository.
+		/// </summary>
+		/// <param name="id">Id of the customer to remove from the repository.</param>
+		/// <returns></returns>
+		public Task DeleteCustomerAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteNote(int id)
+		/// <summary>
+		/// Remove a note from the repository.
+		/// </summary>
+		/// <param name="id">Id of the note to remove from the repository.</param>
+		/// <returns></returns>
+		public Task DeleteNoteAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteUserAccount(int id)
+		/// <summary>
+		/// Remove a user account from the repository.
+		/// </summary>
+		/// <param name="id">Id of the user account to remove from the repository.</param>
+		/// <returns></returns>
+		public Task DeleteUserAccountAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Retrieve a Customer from the repository by Id.
+		/// </summary>
+		/// <param name="id">Id of the customer to retrieve.</param>
+		/// <returns></returns>
 		public Task<Customer> FindCustomerByIdAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Retrieve a Note from the repository by Id.
+		/// </summary>
+		/// <param name="id">Id of the note to retrieve.</param>
+		/// <returns></returns>
 		public Task<Note> FindNoteByIdAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Retrieve a UserAccount from the repository by Id.
+		/// </summary>
+		/// <param name="id">Id of the UserAccount to retrieve.</param>
+		/// <returns></returns>
 		public Task<UserAccount> FindUserAccountByIdAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<List<Customer>> GetAllCustomers(Expression<Func<bool>> expression)
+		/// <summary>
+		/// Retrieve all Customer objects from the repostory that match the given expression.
+		/// </summary>
+		/// <param name="expression">Expression that returns true for Customer objects to retrieve.</param>
+		/// <returns></returns>
+		public Task<List<Customer>> GetAllCustomersAsync(Expression<Func<bool>> expression)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Retrieve all Customer objects from the repository.
+		/// </summary>
+		/// <returns></returns>
 		public Task<List<Customer>> GetAllCustomersAsync()
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<List<Note>> GetAllNotes(Expression<Func<bool>> expression)
+		/// <summary>
+		/// Retrieve all Note objects from the repository that match the given expression.
+		/// </summary>
+		/// <param name="expression">Expression that returns true for Customer objects to retrieve.</param>
+		/// <returns></returns>
+		public Task<List<Note>> GetAllNotesAsync(Expression<Func<bool>> expression)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Retrieve all Note objects from the repository.
+		/// </summary>
+		/// <returns></returns>
 		public Task<List<Note>> GetAllNotesAsync()
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<List<UserAccount>> GetAllUserAccounts(Expression<Func<bool>> expression)
+		/// <summary>
+		/// Retrieve all UserAccount objects from the repository that match the given expression.
+		/// </summary>
+		/// <param name="expression"></param>
+		/// <returns></returns>
+		public Task<List<UserAccount>> GetAllUserAccountsAsync(Expression<Func<bool>> expression)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Retrieve all UserAccount objects from the repository.
+		/// </summary>
+		/// <returns></returns>
 		public Task<List<UserAccount>> GetAllUserAccountsAsync()
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task UpdateCustomer(Customer customer)
+		/// <summary>
+		/// Update a Customer in the repository that has the matching Id with modified property values.
+		/// </summary>
+		/// <param name="customer">Customer object that contains the modified property values to update with.</param>
+		/// <returns></returns>
+		public Task UpdateCustomerAsync(Customer customer)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task UpdateNote(Note userAccount)
+		/// <summary>
+		/// Update a Note in the repository that has the matching Id with modified property values.
+		/// </summary>
+		/// <param name="note">Note object that contains the modified property values to update with.</param>
+		/// <returns></returns>
+		public Task UpdateNoteAsync(Note note)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task UpdateUserAccount(UserAccount userAccount)
+		/// <summary>
+		/// Update a UserAccount in the repository that has the matching Id with modified property values.
+		/// </summary>
+		/// <param name="userAccount">UserAccoubt object that contains the modified property values to update with.</param>
+		/// <returns></returns>
+		public Task UpdateUserAccountAsync(UserAccount userAccount)
 		{
 			throw new NotImplementedException();
 		}
