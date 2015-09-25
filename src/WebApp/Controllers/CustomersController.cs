33f603d7edf3ad653c79c84850f6a4021017e0d5
Mason McGlothlin <mason.mcglothlin@gmail.com>
2015-09-24 20:35:03 -0500

    Added Ninject for MVC to the project, created basic list of customers page.

---
+++ src/WebApp/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MasonOgCRM.DataAccess.Common;
+
+namespace MasonOgCRM.WebApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+		private readonly IOgCRMRepository Repository;
+
+		public CustomersController(IOgCRMRepository repository)
+		{
+			Repository = repository;
+		}
+
+        public async Task<ActionResult> Index()
+        {
+            return View(await Repository.GetAllCustomersAsync());
+        }
+    }
+}
