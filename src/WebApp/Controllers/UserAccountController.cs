517d9fc0205c9e0162655bd4f1efe3a392d98348
Mason McGlothlin <mason.mcglothlin@gmail.com>
2015-09-28 18:35:47 -0500

    Basic authorization of the site implemented with Forms Auth. Still need to do password authentication, improve the look of the login and register pages, and test registration.

--- src/WebApp/Controllers/UserAccountController.cs
+++ src/WebApp/Controllers/UserAccountController.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MasonOgCRM.DataAccess.Common;
 using MasonOgCRM.DomainModels;
+using MasonOgCRM.WebApp.Helpers;
 
 namespace MasonOgCRM.WebApp.Controllers
 {
-    public class UserAccountController : Controller
-    {
+	public class UserAccountController : Controller
+	{
 		private readonly IOgCRMRepository Repository;
 
 		public UserAccountController(IOgCRMRepository repository)
@@ -19,9 +21,9 @@ namespace MasonOgCRM.WebApp.Controllers
 		}
 
 		public ActionResult Index()
-        {			
-            return View(Repository.GetAllUserAccounts());
-        }
+		{
+			return View(Repository.GetAllUserAccounts());
+		}
 
 		public ActionResult New()
 		{
@@ -34,9 +36,37 @@ namespace MasonOgCRM.WebApp.Controllers
 			return RedirectToAction(nameof(UserAccountController.Index));
 		}
 
+		[AllowAnonymous]
 		public ActionResult Login()
 		{
 			return View();
+		}
+
+		[AllowAnonymous]
+		public ActionResult Register() {
+			return View();
+		}
+
+		[AllowAnonymous]
+		[HttpPost]
+		public ActionResult Login(string returnUrl)
+		{
+			FormsAuthentication.SetAuthCookie("username", false);
+			return Redirect(returnUrl ?? Url.Action(nameof(HomeController.Index), nameof(HomeController).RemoveControllerFromName()));
+		}
+
+		[AllowAnonymous]
+		[HttpPost]
+		public ActionResult Register(UserAccount account)
+		{
+			Repository.AddUserAccount(account);
+			return RedirectToAction(nameof(UserAccountController.Index));
+		}
+
+		public ActionResult PerformLogout()
+		{
+			FormsAuthentication.SignOut();
+			return RedirectToAction(nameof(UserAccountController.Login));
 		}
 	}
 }
--- src/WebApp/Helpers/RazorPageHelperExtensions.cs
+++ src/WebApp/Helpers/RazorPageHelperExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MasonOgCRM.WebApp.Razor
+namespace MasonOgCRM.WebApp.Helpers
 {
 	public static class RazorPageHelperExtensions
 	{
